1530252b965102e74bdfd8ce49f1006cbc464d74
Raul Oliva Escribano <raul.oliva.escribano@gmail.com>
2019-11-12 12:14:29 +0100

    user feature

--- ethereum-truffle/contracts/Notary.sol
+++ ethereum-truffle/contracts/Notary.sol
@@ -6,48 +6,39 @@ contract Notary {
       bytes32 doc;
       uint time;
       bool exist;
-      //User creator;
+      string user;
   }
-
-  /*struct User {
-      string name;
-      string surName;
-  }*/
 
   mapping(bytes32 => DocumentSaved) private documents;
   mapping (bytes32 => bool) private proofs;
 
-  function storeProof(bytes32 proof) private {
+  function storeProof(bytes32 proof, string memory dni) private {
     proofs[proof] = true;
     DocumentSaved storage document = documents[proof];
     document.doc = proof;
     document.exist = true;
-    document.time = block.timestamp;
+    document.user = dni;
+    document.time = block.timestamp; //conversion to normal date https://www.epochconverter.com/
   }
 
-  function notarize(string memory document) public {
-    if (checkDocument(document)){
-      //all user data
-    } else {
-      //not all user data
-    }
+  function notarize(string memory document, string memory dni) public {
     bytes32 proof = proofFor(document);
-    storeProof(proof);
+    storeProof(proof, dni);
   }
 
   function proofFor(string memory document) private pure returns (bytes32) {
     return sha256(abi.encode(document));
   }
 
-  function checkDocument(string memory document) public view returns (bool) {
+  function checkDocument(string memory document) public view returns (DocumentSaved memory) {
     bytes32 proof = proofFor(document);
 
     return hasProof(proof);
   }
 
-  function hasProof(bytes32 proof) private view returns(bool) {
+  function hasProof(bytes32 proof) private view returns(DocumentSaved memory) {
     DocumentSaved memory doc = documents[proof];
-    return doc.exist;
+    return doc;
   }
 
 }
